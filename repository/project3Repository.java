d727288817cc001e4b9562feb706ec6199c901a8
Hemanth1210 <113286630+Hemanth1210@users.noreply.github.com>
2023-08-04 16:21:04 +0530

    Add files via upload

---
+++ repository/project3Repository.java
@@ -0,0 +1,9 @@
+package com.example.project3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.project3.model.project3;
+
+public interface project3Repository extends JpaRepository<project3, Long> {
+    
+}
